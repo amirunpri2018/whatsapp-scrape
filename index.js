08b2aeb8762da69cee58b09abf7aed21f6b45ada
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2019-02-18 14:16:43 +0700

    remove auth middleware

--- index.js
+++ index.js
@@ -2,16 +2,10 @@ const fastify = require('fastify');
 require('axios');
 require('cheerio');
 const { scrapeRoute } = require('./src/routes/Scrape');
-const { basicAuth, basicAuthHook } = require('./src/routes/Auth');
 const { port } = require('./env');
 
 const server = fastify();
-const basicAuthPlugin = basicAuth();
-server.register(basicAuthPlugin.plugin, basicAuthPlugin.options).after(() => {
-    const hook = basicAuthHook(server);
-    server.addHook(hook.name, hook.middleware);
-    server.route(scrapeRoute);
-});
+server.route(scrapeRoute);
 server.listen(parseInt(port, 10), (err, address) => {
     if (err) {
         // eslint-disable-next-line no-console
